85d6784a87164ac1770a0bf3d9cd22800c39eb94
Daniel Sloof <daniel@rubic.nl>
2022-11-23 21:25:16 +0100

    make it simple

---
+++ go.mod
@@ -0,0 +1,17 @@
+module main
+
+go 1.19
+
+require (
+	github.com/charmbracelet/wish v0.7.0
+	github.com/gliderlabs/ssh v0.3.5
+)
+
+require (
+	github.com/anmitsu/go-shlex v0.0.0-20200514113438-38f4b401e2be // indirect
+	github.com/caarlos0/sshmarshal v0.1.0 // indirect
+	github.com/charmbracelet/keygen v0.3.0 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	golang.org/x/crypto v0.0.0-20220826181053-bd7e27e6170d // indirect
+	golang.org/x/sys v0.0.0-20220825204002-c680a09ffe64 // indirect
+)
--- main.go
+++ main.go
@@ -1,50 +1,16 @@
 package main
 
 import (
-	"github.com/charmbracelet/wish"
-	lm "github.com/charmbracelet/wish/logging"
-	"github.com/gliderlabs/ssh"
+	"io"
 	"log"
-	"net"
-	"os"
-	"os/signal"
-	"syscall"
-	"time"
+
+	"github.com/gliderlabs/ssh"
 )
 
 func main() {
+	ssh.Handle(func(s ssh.Session) {
+		io.WriteString(s, "Hello world\n")
+	})
 
-	// SSH server begin
-	s, err := wish.NewServer(
-		ssh.PasswordAuth(passHandler),
-		ssh.PublicKeyAuth(pkHandler),
-		wish.WithAddress(":2222"),
-		wish.WithMiddleware(
-			lm.Middleware(),
-			loginBubbleteaMiddleware(),
-		),
-	)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	s.ConnectionFailedCallback = func(conn net.Conn, err error) {
-		log.Println("Connection failed:", err)
-	}
-	done := make(chan os.Signal, 0)
-	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
-	log.Printf("Starting SSH server on %s:%d", host, port)
-	go func() {
-		err = s.ListenAndServe()
-		if err != nil {
-			log.Fatalln(err)
-		}
-	}()
-	// start web based terminal
-	<-done
-	log.Println("Stopping SSH server")
-	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
-	defer func() { cancel() }()
-	if err := s.Shutdown(ctx); err != nil {
-		log.Fatalln(err)
-	}
+	log.Fatal(ssh.ListenAndServe(":2222", nil))
 }
